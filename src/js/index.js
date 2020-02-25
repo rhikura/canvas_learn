d08b1c29748c2119ef41febb1e9e04de793d2d4f
Ivan Zakharin <r.hikura@yandex.com>
2020-02-25 07:02:59 +0200

    #T0
    Let draw simple canvas

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import "../css/style.css";
+
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+canvas.width = GAME_WIDTH;
+canvas.height = GAME_HEIGHT;
